e4d485fa9c9b1f0b253ff637201955e6808bbcb5
AMJL-16 <ozaurelio@gmail.com>
2023-05-08 01:31:41 +0000

    Add name of the game and rules

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print('--------------------------------')
+print("|   WORLD CAPITALS QUIZ GAME   |")
+print('--------------------------------\n')
+print("********************************")
+print('')
+print("Rules of the Quiz:")
+print('To play answer each questions and press enter!')
+print('If you answer correctly you will score one point if wrong Zero,')
+print('then you will go to the next question.')
+print('Your score is displayed after each question.')
+print('')
+print("********************************")
+
