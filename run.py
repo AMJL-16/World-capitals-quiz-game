146571901ce53a21136a3baa0c4f3c43b3735bf8
AMJL-16 <ozaurelio@gmail.com>
2023-05-08 01:46:57 +0000

    Add the dictionary for the quiz

--- run.py
+++ run.py
@@ -14,3 +14,34 @@ print('then you will go to the next question.')
 print('Your score is displayed after each question.')
 print('')
 print("********************************")
+playing = input("Do you want to play?  ")
+if playing.lower() != "yes":
+    quit()
+
+name = input('Cool, enter your Name to start the game: ')
+print("")
+print(f"Hello, {name} have fun!\n")
+
+# building the dictionary with countries and capitals as a gobal variable
+world_capital = {
+    "France": "Paris",
+    "Bostwana": "Gaborone",
+    "Spain": "Madrid",
+    "Australia": "Canberra",
+    "Bahamas": "Nassau",
+    "Peru": "Lima",
+    "Monaco": "Monaco",
+    "Malaysia": "Kuala Lumpur",
+    "Ireland ": "Dublin",
+    "Belgium": "Brussels",
+    "Mauritius": "Port Louis",
+    "Italy": "Rome",
+    "Vietnam ": "Hanoi",
+    "Qatar": "Doha",
+    "Seychelles": "Victoria",
+    "Bulgaria ": "Sofia",
+    "Croatia ": "Zagreb",
+    "Senegal ": "Dakar",
+    "Thailand ": "Bangkok",
+    "Lebanon ": "Beirut"
+           }
